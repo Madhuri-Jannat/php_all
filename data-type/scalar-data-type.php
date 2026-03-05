d0c85c525744b98a7d2fdd9c9eac4713b349f06a
Madhuri-Jannat <iftiytuljannat@gmail.com>
2026-03-05 10:57:36 +0600

    datatype

---
+++ data-type/scalar-data-type.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>scalar data type</title>
+</head>
+<body>
+    <?php
+    // string
+    $data = "Hello Bangladesh";
+    echo $data;
+    echo "<br>";
+    var_dump($data);
+
+    // boolean 
+    $vtype = true;
+    echo "<br>";
+    var_dump($vtype);
+
+    // intergar
+    $int = 44;
+    echo "<br>";
+    var_dump($int);
+
+
+    // floot
+    $f = 34.5089;
+    echo "<br>";
+    var_dump($f);
+    ?>
+</body>
+</html>
---
+++ home1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $num1 = 34;
+    $num2 = 7;
+    $sum =$num1+$num2;
+   echo $sum;
+    ?>
+</body>
+</html>
