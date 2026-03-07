bd47901b56ec3971b69892d3bba0ba8722b89806
Madhuri-Jannat <iftiytuljannat@gmail.com>
2026-03-07 12:04:25 +0600

    statement

---
+++ statement/else-if-statement.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a =23;
+    $b=150;
+    $c=6;
+      if($a >$b) {
+        echo "this is true";
+      } else if ($b>$c){
+        echo "print b";
+      }else "not print";
+    ?>
+</body>
+</html>
